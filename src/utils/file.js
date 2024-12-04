989157fef9524e05fb4c5f09f50a50a8b91dd8a6
Alazizxan <azikcanpapagul@gmail.com>
2024-12-04 19:56:48 +0500

    ishlamadi eruda eror

--- src/utils/file.js
+++ src/utils/file.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const TELEGRAM_BOT_TOKEN = '8159380099:AAG7qlQpYgD7YjR6G7jjeOSc6JD5mOBideA'; 
+const TELEGRAM_BOT_TOKEN = "8159380099:AAG7qlQpYgD7YjR6G7jjeOSc6JD5mOBideA"; 
 
 export default async function getFileLink(fileId) {
     const response = await axios.get(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/getFile?file_id=${fileId}`);
